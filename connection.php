aeb0d7d7f1013a79098ce0914dcfea887e750c0e
Ankit Upadhyay <72919994+Ankkkitt@users.noreply.github.com>
2021-10-02 21:33:21 +0530

    remote mysql added

--- connection.php
+++ connection.php
@@ -1,8 +1,16 @@
 <?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$db = "diary";
+// development part
+// $server = "localhost";
+// $user = "root";
+// $password = "";
+// $db = "diary";
+
+// remote mysql connection
+$server = "remotemysql.com";
+$user = "qfrht2vMKA";
+$password = "M32P3uY7SS";
+$db = "qfrht2vMKA";
+
 
 $con = mysqli_connect($server, $user, $password, $db);
 if($con){
